11c618c551c913e1b73365db3ef7de4854730a6e
drewzambelli <drewzambelli@gmail.com>
2025-04-24 21:50:06 -0400

    Configure for GitHub Pages deployment with HashRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 // index.js - Main entry point
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom'; // Change to HashRouter
+
 import './index.css';
 import App from './App';
 import MemberDetail from './MemberDetail';
@@ -9,11 +10,36 @@ import MemberDetail from './MemberDetail';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/member/:id" element={<MemberDetail />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
+
+
+
+
+
+
+// // index.js - Main entry point
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import './index.css';
+// import App from './App';
+// import MemberDetail from './MemberDetail';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter basename={process.env.PUBLIC_URL}>
+//       <Routes>
+//         <Route path="/" element={<App />} />
+//         <Route path="/member/:id" element={<MemberDetail />} />
+//       </Routes>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
